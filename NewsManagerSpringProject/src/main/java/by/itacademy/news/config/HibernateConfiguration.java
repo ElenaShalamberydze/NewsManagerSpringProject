0c8f2f55940dc4ed2572e2ca51a4bb45918df531
Elena <56275475+ElenaShalamberydze@users.noreply.github.com>
2021-01-16 10:51:36 +0300

    Update HibernateConfiguration.java

--- NewsManagerSpringProject/src/main/java/by/itacademy/news/config/HibernateConfiguration.java
+++ NewsManagerSpringProject/src/main/java/by/itacademy/news/config/HibernateConfiguration.java
@@ -30,21 +30,18 @@ public class HibernateConfiguration {
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
 
         Properties props = new Properties();
-
-        // Setting JDBC properties
+        
         props.put(DRIVER, env.getProperty("mysql.driver"));
         props.put(URL, env.getProperty("mysql.url"));
         props.put(USER, env.getProperty("mysql.user"));
         props.put(PASS, env.getProperty("mysql.password"));
 
-        // Setting C3P0 properties
         props.put(C3P0_MIN_SIZE, env.getProperty("hibernate.c3p0.min_pool_size"));
         props.put(C3P0_MAX_SIZE, env.getProperty("hibernate.c3p0.max_pool_size"));
         props.put(C3P0_ACQUIRE_INCREMENT, env.getProperty("hibernate.c3p0.acquire_increment"));
         props.put(C3P0_TIMEOUT, env.getProperty("hibernate.c3p0.timeout"));
         props.put(C3P0_MAX_STATEMENTS, env.getProperty("hibernate.c3p0.max_statements"));
 
-        // Setting Hibernate properties
         props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
         props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
         factoryBean.setHibernateProperties(props);
